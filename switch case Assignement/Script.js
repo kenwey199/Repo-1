bddb8c5d51ab4c71d15e5de6a799999e5fe425a5
Mohamed Attia <kenweu@gmail.com>
2024-08-11 15:46:29 +0200

    Switch Assignement

---
+++ switch case Assignement/Script.js
@@ -0,0 +1,17 @@
+let userGov = prompt("Enter your Gov")
+
+switch (userGov) {
+    case "cairo":
+        alert("5%");
+        break
+    case "alex":
+        alert("10%");
+        break
+    case "aswan":
+        alert("30%");
+        break
+
+    default:
+        alert("40%");
+
+} 
